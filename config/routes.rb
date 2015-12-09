b22ad6083355e3fc35b722e373fbbacd1b383a86
John McIntosh II <jmci10@hotmail.com>
2015-12-09 21:12:12 +0000

    update user profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   
-  get 'welcome/index'
-  
   resources :users, only: [:show] do 
   end
   
-  root to:'users#show', as: :user_root
+  root to:'welcome#index'
 
 end
