6d7636f7ccb66add5f15f6df7673e9a912e8c56e
John McIntosh II <jmci10@hotmail.com>
2015-12-13 04:31:12 +0000

    user items rendering

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,25 @@
 class ItemsController < ApplicationController
   
+  def new
+    @item = Item.new  
+  end
+  
   def create
-    @item = Item.new
-    @item.name = params[:item][:name]
+    @item = current_user.items.build(items_params)
+    
+    if @item.save
+      flash[:notice] = "Objective successfully added."
+      redirect_to user_path(current_user)
+    else
+      flash[:notice] = "Objective failed to add."
+      redirect_to user_path(current_user)
+    end 
+  end
+  
+  private
+  
+  def items_params
+    params.require(:item).permit(:name)
   end
   
 end
